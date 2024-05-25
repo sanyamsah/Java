528609b2ee8b9d9a8c0b6c752bd2ed145281f321
sanyamsah <sanyamsah25@gmail.com>
2024-05-25 19:12:59 +0530

    modified

--- Recursion/Sorted.java
+++ Recursion/Sorted.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Sorted {
     private static boolean sorted(int[] arr, int i){
         if(i==arr.length-1) return true;
@@ -5,13 +7,15 @@ public class Sorted {
     }
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6}; // sorted
-        int[] brr = {1,2,3,4,6,5}; // not sorted
+        int[] brr = {1,2,3,4,6,5};// not sorted
+        System.out.print(Arrays.toString(arr) + ": ");
         if(sorted(arr, 0)){
             System.out.println("Sorted");
         }
         else{
             System.out.println("Not sorted");
         }
+        System.out.print(Arrays.toString(brr) + ": ");
         if(sorted(brr, 0)){
             System.out.println("Sorted");
         }
