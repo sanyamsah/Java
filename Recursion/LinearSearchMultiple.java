8be221dc740fa1e7134289113538584d63ddd36f
sanyamsah <sanyamsah25@gmail.com>
2024-05-25 20:03:41 +0530

    linear search

--- Recursion/LinearSearchMultiple.java
+++ Recursion/LinearSearchMultiple.java
@@ -10,6 +10,11 @@ public class LinearSearchMultiple {
             if(arr[i]==key) list.add(i);
             search(arr,key,++i);
         }
+        private static ArrayList<Integer> search(int[] arr, int key, int i, ArrayList<Integer> l){
+            if(i==arr.length) return l;
+            if(arr[i]==key) l.add(i);
+            return search(arr,key,++i,l);
+        }
         public static void main(String[] args) {
             Scanner input = new Scanner(System.in);
             int[] arr = {1,5,8,9,3,0,6,7,9,8,10,13,13,25,99};
@@ -19,5 +24,9 @@ public class LinearSearchMultiple {
             search(arr,key,0);
             if(list.isEmpty()) System.out.println("Element not found..");
             else System.out.println("Element found at index: " + list);
+            ArrayList<Integer> l = search(arr,key,0,new ArrayList<>());
+            System.out.println(l);
+            if(l.isEmpty()) System.out.println("Element not found..");
+            else System.out.println("Element found at index: " + l);
         }
 }
