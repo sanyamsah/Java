ed8dfff984b8065ae0bfb42e1e383e682dea82d5
sanyamsah <sanyamsah25@gmail.com>
2024-08-12 15:58:49 +0530

    palindrome

--- Recursion/Palindrome.java
+++ Recursion/Palindrome.java
@@ -1,14 +1,23 @@
 import java.util.Scanner;
 
 public class Palindrome {
-    private static long reverse(long n, long rev){
-        if(n == 0) return rev;
+//    private static long reverse(long n, long rev){
+//        if(n == 0) return rev;
+//        long rem = n % 10;
+//        rev = rev * 10 + rem;
+//        return reverse(n/10, rev);
+//    }
+//    private static boolean isPalindrome(long n){
+//        return (n == reverse(n, 0));
+//    }
+    private static long reverse(long n, long digits){
+        if(n % 10 == n) return n;
         long rem = n % 10;
-        rev = rev * 10 + rem;
-        return reverse(n/10, rev);
+        return rem * (long) Math.pow(10, digits-1) + reverse(n/10, digits-1);
     }
-    private static boolean isPalindrome(long n){
-        return (n == reverse(n, 0));
+    private static boolean isPalindrome(long num){
+        long digits = (long) Math.log10(num) + 1;
+        return (num == reverse(num, digits));
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
