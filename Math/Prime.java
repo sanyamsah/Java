21cac07cb87981c7408d65b588649d89ec7b79a8
sanyamsah <sanyamsah25@gmail.com>
2024-08-15 23:56:00 +0530

    number prime or composite

--- Math/Prime.java
+++ Math/Prime.java
@@ -1,18 +1,15 @@
-import java.util.Scanner;
-
 public class Prime {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int num = input.nextInt();
-        if (isPrime(num)) {
-            System.out.println("Prime");
-        } else {
-            System.out.println("Not Prime");
+        for(int i = 0; i <= 30; i++){
+            if (isPrime(i)) {
+                System.out.println(i + ": Prime");
+            } else {
+                System.out.println(i + ": Composite");
+            }
         }
     }
     private static boolean isPrime(int num) {
-        if(num == 0 || num == 1) return false;
+        if(num <= 1) return false;
         for(int i = 2; i <= (int)Math.sqrt(num); i++){
             if(num % i == 0) return false;
         }
