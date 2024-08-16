9d468edc96bc39c99347124a04aaf1949d5efe3f
sanyamsah <sanyamsah25@gmail.com>
2024-08-16 18:35:49 +0530

    file io

--- File Handeling/FileReadWrite.java
+++ File Handeling/FileReadWrite.java
@@ -1,15 +1,24 @@
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileReadWrite {
     public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
         try{
             FileReader fr = new FileReader("demo.txt");
-            int letter = fr.read();
-            while(fr.ready()){
-                System.out.print((char)letter);
-                letter = fr.read();
+            int letter;
+            while((letter = fr.read()) != -1){
+                sb.append((char)letter);
             }
+            System.out.println(sb);
+            fr.close();
+        } catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+        try(FileWriter fw = new FileWriter("demo2.txt", true)){ // append: true
+            fw.write("\n");
+            fw.write("Hello, World");
         } catch(IOException e){
             System.out.println(e.getMessage());
         }
