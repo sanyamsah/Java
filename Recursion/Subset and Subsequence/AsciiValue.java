1df60f96be3184278ff7e45290bf2c63f1fad017
sanyamsah <sanyamsah25@gmail.com>
2024-08-18 17:22:25 +0530

    ascii

--- Recursion/Subset and Subsequence/AsciiValue.java
+++ Recursion/Subset and Subsequence/AsciiValue.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class AsciiValue {
+public class AsciiValue {
+    public static void main(String[] args) {
+        char ch = 97;
+        System.out.println(ch); // a
+        System.out.println(ch + 1); // 98
+        System.out.println((char)(ch + 1)); // b
+
+    }
 }
