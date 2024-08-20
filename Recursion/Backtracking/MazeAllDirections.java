78a9ce3b210b451f42c7e17f121a3c8f223c512b
sanyamsah <sanyamsah25@gmail.com>
2024-08-20 23:22:54 +0530

    backtracking

--- Recursion/Backtracking/MazeAllDirections.java
+++ Recursion/Backtracking/MazeAllDirections.java
@@ -16,38 +16,67 @@ public class MazeAllDirections {
         System.out.println("Paths: " + pathList(maze, 0, 0, ""));
     }
     private static ArrayList<String> pathList(boolean[][] maze, int r, int c, String path){
+        ArrayList<String> paths = new ArrayList<>();
         if(r == maze.length - 1 && c == maze[0].length - 1){
-            ArrayList<String> list = new ArrayList<>();
-            list.add(path);
-            return list;
+            paths.add(path);
+            return paths;
         }
+        if(!maze[r][c]){
+            return paths;
+        }
+        maze[r][c] = false;
         ArrayList<String> down = new ArrayList<>();
         ArrayList<String> right = new ArrayList<>();
         ArrayList<String> up = new ArrayList<>();
         ArrayList<String> left = new ArrayList<>();
-        if(r < maze.length - 1 && maze[r][c]){
-            maze[r][c] = false;
-            down = pathList(maze, r + 1, c, path + "D");
-            maze[r][c] = true;
+        if(r < maze.length - 1){
+            paths.addAll(pathList(maze, r + 1, c, path + "D"));
         }
-        if(c < maze[0].length - 1 && maze[r][c]){
-            maze[r][c] = false;
-            right = pathList(maze, r, c + 1, path + "R");
-            maze[r][c] = true;
+        if(c < maze[0].length - 1){
+            paths.addAll(pathList(maze, r, c + 1, path + "R"));
         }
-        if(r > 0 && maze[r][c]){
-            maze[r][c] = false;
-            up = pathList(maze, r - 1, c, path + "U");
-            maze[r][c] = true;
+        if(r > 0){
+            paths.addAll(pathList(maze, r - 1, c, path + "U"));
         }
-        if(c > 0 && maze[r][c]){
-            maze[r][c] = false;
-            left = pathList(maze, r, c - 1, path + "L");
-            maze[r][c] = true;
+        if(c > 0){
+            paths.addAll(pathList(maze, r, c - 1, path + "L"));
         }
-        down.addAll(right);
-        down.addAll(up);
-        down.addAll(left);
-        return down;
+        maze[r][c] = true;
+        return paths;
     }
+//    private static ArrayList<String> pathList(boolean[][] maze, int r, int c, String path){
+//        if(r == maze.length - 1 && c == maze[0].length - 1){
+//            ArrayList<String> list = new ArrayList<>();
+//            list.add(path);
+//            return list;
+//        }
+//        ArrayList<String> down = new ArrayList<>();
+//        ArrayList<String> right = new ArrayList<>();
+//        ArrayList<String> up = new ArrayList<>();
+//        ArrayList<String> left = new ArrayList<>();
+//        if(r < maze.length - 1 && maze[r][c]){
+//            maze[r][c] = false;
+//            down = pathList(maze, r + 1, c, path + "D");
+//            maze[r][c] = true;
+//        }
+//        if(c < maze[0].length - 1 && maze[r][c]){
+//            maze[r][c] = false;
+//            right = pathList(maze, r, c + 1, path + "R");
+//            maze[r][c] = true;
+//        }
+//        if(r > 0 && maze[r][c]){
+//            maze[r][c] = false;
+//            up = pathList(maze, r - 1, c, path + "U");
+//            maze[r][c] = true;
+//        }
+//        if(c > 0 && maze[r][c]){
+//            maze[r][c] = false;
+//            left = pathList(maze, r, c - 1, path + "L");
+//            maze[r][c] = true;
+//        }
+//        down.addAll(right);
+//        down.addAll(up);
+//        down.addAll(left);
+//        return down;
+//    }
 }
