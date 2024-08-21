20676d8372e1965d13783ae7e42549819e183baa
sanyamsah <sanyamsah25@gmail.com>
2024-08-21 21:52:52 +0530

    N-Queens problem

---
+++ Recursion/Backtracking/N_Queens.java
@@ -0,0 +1,50 @@
+public class N_Queens {
+    public static void main(String[] args) {
+        int n = 4;
+        boolean[][] board = new boolean[n][n];
+        System.out.println("Number of ways: " + queens(board, 0));
+    }
+    private static int queens(boolean[][] board, int row){
+        if(row == board.length){
+            display(board);
+            return 1;
+        }
+        int count = 0;
+        for(int col = 0; col < board.length; col++){
+            if(isSafe(board, row, col)){
+                board[row][col] = true;
+                count += queens(board, row + 1);
+                board[row][col] = false;
+            }
+        }
+        return count;
+    }
+    private static boolean isSafe(boolean[][] board, int row, int col){
+        for(int i = 0; i < row; i++){
+            if(board[i][col]) return false;
+        }
+        // Diagonally left
+        int maxLeft = Math.min(row, col);
+        for(int i = 1; i <= maxLeft; i++){
+            if(board[row - i][col - i]) return false;
+        }
+        int maxRight = Math.min(row, board.length - 1 - col);
+        for(int i = 1; i <= maxRight; i++){
+            if(board[row - i][col + i]) return false;
+        }
+        return true;
+    }
+    private static void display(boolean[][] board){
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board.length; j++){
+                if(board[i][j]){
+                    System.out.print("Q ");
+                } else{
+                    System.out.print("X ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
