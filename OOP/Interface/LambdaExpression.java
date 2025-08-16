fc40297319b8f4a26cdb7aaa3b3a4b1f5dd56aba
Sanyam <83174714+sanyamsah@users.noreply.github.com>
2025-08-16 10:28:05 +0000

    modified LambdaExpression class

--- OOP/Interface/LambdaExpression.java
+++ OOP/Interface/LambdaExpression.java
@@ -3,14 +3,51 @@ interface OuterClass{
     void show();
 }
 
+@FunctionalInterface
+interface ClassWithValue{
+    void showValue(int i);
+}
+
+@FunctionalInterface
+interface Add{
+    int add(int a, int b);
+}
+
 public class LambdaExpression {
     public static void main(String[] args) {
         // OuterClass obj = () -> System.out.println("In lambda expression show..");
         // obj.show();
+
         OuterClass obj = () -> {
-            System.out.println("In lambda expression show..");
             System.out.println("Hello!");
+            System.out.println("In lambda expression show..");
         };
         obj.show();
+
+        // ClassWithValue obj2 = (int i) -> {
+        //     System.out.println("Hello!");
+        //     System.out.println("The passed value is: " + i);
+        // };
+
+        // ClassWithValue obj2 = (i) -> {
+        //     System.out.println("Hello!");
+        //     System.out.println("The passed value is: " + i);
+        // };
+
+        ClassWithValue obj2 = i -> {
+            System.out.println("Hello!");
+            System.out.println("The passed value is: " + i);
+        };
+        obj2.showValue(25);
+
+        // Add obj3 = (int a, int b) -> {
+        //     return a + b;
+        // };
+        // int result = obj3.add(2,5);
+        // System.out.println("Sum = " + result);
+
+        Add obj3 = (a,b) -> a + b;
+        int result = obj3.add(2,5);
+        System.out.println("Sum = " + result);
     }
 }
